715905b3038166a1ed7e4be8217131c49d555581
DimkaFPM <dim3aaa@mail.ru>
2016-10-16 18:06:10 +0300

    equation fixes

--- EquationPackage.m
+++ EquationPackage.m
@@ -4,21 +4,19 @@ getEquation::usage = "create equation string from parameter";
 
 Begin["`Private`"];
 
-getEquation[arr_] := Block [{r, equation, param, i, j, k, a },
-  leftParts = List[];
-
+getEquation[arr_] :=
+ Block [{r, equation, param, leftParts = {}, i, j, k, a , eq},
   For[i = 1, i <= Length[arr], i++,
-    eq = arr[[i]];
-    equation = 0;
-    For[j = 1, j <= Length[eq], j++,
-         param = eq[[j]];
-
-      equation +=
-       param[[1]]*Subscript[x[param[[2]]], param[[3]], param[[4]]]
-      ]
-     AppendTo[leftParts, equation];
-    ]
-   	Return leftParts
+   equation = 0;
+   eq = arr[[i]];
+   For[j = 1, j <= Length[eq], j++,
+       param = eq[[j]];
+       equation +=
+     param[[1]]* Subscript[x[param[[2]]], param[[3]], param[[4]]];
+    ];
+   AppendTo[leftParts, equation];
+   ];
+  Return[leftParts]
   ]
 
 End[]
