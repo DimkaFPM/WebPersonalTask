c2ee5ac99eeebb2155d90f3eff36be96be844d2b
DimkaFPM <dim3aaa@mail.ru>
2016-10-16 14:12:48 +0300

    add arguments for Lab23Solver

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) throws MathLinkException {
         KernelLink kernelLink = createKernelLink();
 
-        testWorkWithTestArrayPackage(kernelLink);
-        testWorkWithLabs23SolverPackage(kernelLink);
+        testTestArrayPackage(kernelLink);
+        testLabs23SolverPackage(kernelLink);
     }
 
     private static KernelLink createKernelLink() throws MathLinkException {
@@ -20,7 +20,7 @@ public class Main {
         return kernelLink;
     }
 
-    private static void testWorkWithTestArrayPackage(KernelLink kernelLink) throws MathLinkException {
+    private static void testTestArrayPackage(KernelLink kernelLink) throws MathLinkException {
         kernelLink.evaluate("<<" + "TestArray.m");//подключаем пакет
         kernelLink.discardAnswer();// дожидаемся загрузки
 
@@ -36,14 +36,67 @@ public class Main {
         System.out.println(String.format("Sum: %d", result));
     }
 
-    private static void testWorkWithLabs23SolverPackage(KernelLink kernelLink) throws MathLinkException {
+    private static void testLabs23SolverPackage(KernelLink kernelLink) throws MathLinkException {
         kernelLink.evaluate("<<" + "Labs23Solver.m");//подключаем пакет
         kernelLink.discardAnswer();// дожидаемся загрузки
 
         kernelLink.putFunction("EvaluatePacket", 1);
 
+        kernelLink.putFunction("SolveLabs23", 8);
+        kernelLink.put(4);//ks
+        kernelLink.put(6);//is
+        kernelLink.put(createU());
+        kernelLink.put(createUt());
+        kernelLink.put(createUc());
+        kernelLink.put(createA());
+        //TODO add leftParts as argument
+        kernelLink.put(createRightParts());
+
         kernelLink.endPacket();
         kernelLink.waitForAnswer();
+
+        //TODO result output
     }
 
+    private static int[][][] createU() {
+        return new int[][][]{
+                {{1, 3}, {2, 1}, {2, 6}, {3, 4}, {3, 6}, {4, 6}, {5, 4}, {6, 5}},
+                {{1, 3}, {1, 4}, {1, 5}, {1, 6}, {2, 6}, {3, 2}, {3, 4}, {4, 6}},
+                {{1, 3}, {1, 5}, {2, 1}, {2, 6}, {4, 6}, {5, 2}, {5, 4}, {6, 5}},
+                {{1, 4}, {1, 5}, {1, 6}, {2, 1}, {2, 6}, {3, 2}, {3, 4}, {3, 6}, {4, 6}, {5, 2}, {5, 4}}
+        };
+
+    }
+
+    private static int[][][] createUt() {
+        return new int[][][]{
+                {{2, 1}, {2, 6}, {3, 4}, {4, 6}, {5, 4}},
+                {{1, 3}, {1, 5}, {1, 6}, {2, 6}, {4, 6}},
+                {{1, 3}, {1, 5}, {2, 6}, {4, 6}, {5, 2}},
+                {{1, 4}, {1, 5}, {2, 6}, {3, 2}, {4, 6}}
+        };
+
+    }
+
+    private static int[][][] createUc() {
+        return new int[][][]{
+                {},
+                {{3, 4}},
+                {{2, 1}, {5, 4}},
+                {{3, 4}, {5, 2}}
+        };
+    }
+
+    private static int[][] createA() {
+        return new int[][]{
+                {-3, 16, 9, -11, 3, -14},
+                {26, 2, 6, -5, -6, -23},
+                {7, 4, -1, -7, -2, -1},
+                {5, 8, 11, -15, -2, -7}
+        };
+    }
+
+    private static int[] createRightParts() {
+        return new int[]{853, 908, 11, 17, 15};
+    }
 }
